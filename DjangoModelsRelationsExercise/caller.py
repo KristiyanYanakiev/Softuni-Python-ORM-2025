eb508db6cd17f6fc3743e71cd1c9671786368b38
KristiyanYanakiev <yankris_92@yahoo.com>
2025-11-03 14:51:39 +0200

    more solutions

--- DjangoModelsRelationsExercise/caller.py
+++ DjangoModelsRelationsExercise/caller.py
@@ -1,4 +1,6 @@
 import os
+
+
 import django
 from django.db.models import QuerySet
 
@@ -6,7 +8,10 @@ from django.db.models import QuerySet
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Book, Author, Artist, Song
+from main_app.models import Book, Author, Artist, Song, Product, Review, Driver, DrivingLicense, Owner, Registration, \
+    Car
+from datetime import timedelta, date, datetime
+from typing import List
 
 
 # Create queries within functions
@@ -36,4 +41,44 @@ def remove_song_from_artist(artist_name: str, song_title: str) -> None:
     song = Song.objects.get(title=song_title)
     artist.songs.remove(song)
 
+def calculate_average_rating_for_product_by_name(product_name: str) -> float:
+    product = Product.objects.get(name=product_name)
+
+    return sum(r.rating for r in product.reviews.all()) / len(product.reviews.all())
+
+def get_reviews_with_high_ratings(threshold: int) -> QuerySet[Review]:
+    return Review.objects.filter(rating__gte=threshold)
+
+def get_products_with_no_reviews() -> QuerySet[Product]:
+    return Product.objects.filter(reviews__isnull=True).order_by('-name')
+
+def delete_products_without_reviews() -> None:
+    Product.objects.filter(reviews__isnull=True).delete()
+
+def calculate_licenses_expiration_dates() -> str:
+    return '\n'.join(
+        f"License with number: {l.license_number} expires on {l.issue_date + timedelta(days=365)}!"
+        for l in DrivingLicense.objects.all().order_by('-license_number')
+    )
+
+def get_drivers_with_expired_licenses(due_date: date):
+    res = []
+    for d  in Driver.objects.all():
+        expiration_date = d.licence.issue_date + timedelta(days=365)
+        if expiration_date < due_date:
+            res.append(d)
+
+    return res
+
+def register_car_by_owner(owner: Owner) -> str:
+    registration = Registration.objects.filter(car__isnull=True).first()
+    car = Car.objects.filter(registration__isnull=True).first()
+
+    car.registration = registration
+    car.owner = owner
+    registration.registration_date = datetime.today()
+    car.save()
+    registration.save()
+
+    return f"Successfully registered {car.model} to {car.owner.name} with registration number {car.registration.registration_number}."
 
--- DjangoModelsRelationsExercise/main_app/models.py
+++ DjangoModelsRelationsExercise/main_app/models.py
@@ -1,4 +1,8 @@
+
+
 from django.db import models
+
+from datetime import datetime, timedelta
 
 # Create your models here.
 class Author(models.Model):
@@ -18,4 +22,41 @@ class Song(models.Model):
 class Artist(models.Model):
     name = models.CharField(max_length=100)
     songs = models.ManyToManyField(Song, related_name='artists')
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Review(models.Model):
+    description = models.CharField(max_length=200)
+    rating = models.PositiveSmallIntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class DrivingLicense(models.Model):
+    license_number = models.CharField(max_length=10, unique=True)
+    issue_date = models.DateField()
+    driver = models.OneToOneField(Driver, on_delete=models.CASCADE, related_name='licence')
+
+
+class Owner(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, null=True, blank=True, related_name='cars' )
+
+
+class Registration(models.Model):
+    registration_number = models.CharField(max_length=10, unique=True)
+    registration_date = models.DateField(null=True, blank=True)
+    car = models.OneToOneField(Car, on_delete=models.CASCADE, null=True, blank=True, related_name='registration')
 
