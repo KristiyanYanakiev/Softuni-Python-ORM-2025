dafb4a62e37929346a82c0ea17c232c3c82cefe8
KristiyanYanakiev <yankris_92@yahoo.com>
2025-11-14 16:51:54 +0200

    more solutions

--- AdvancedQueriesInDjango_Exercise/caller.py
+++ AdvancedQueriesInDjango_Exercise/caller.py
@@ -1,10 +1,68 @@
 import os
 import django
 
+
+
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
+from main_app.models import VideoGame
 # Import your models here
+# Create instances of VideoGame with real data
+game1 = VideoGame.objects.create(
+    title="The Last of Us Part II",
+    genre="Action",
+    release_year=2020,
+    rating=9.0
+)
 
-# Run and print your queries
+game2 = VideoGame.objects.create(
+    title="Cyberpunk 2077",
+    genre="RPG",
+    release_year=2020,
+    rating=7.2
+)
+
+game3 = VideoGame.objects.create(
+    title="Red Dead Redemption 2",
+    genre="Adventure",
+    release_year=2018,
+    rating=9.7
+)
+
+game4 = VideoGame.objects.create(
+    title="FIFA 22",
+    genre="Sports",
+    release_year=2021,
+    rating=8.5
+)
+
+game5 = VideoGame.objects.create(
+    title="Civilization VI",
+    genre="Strategy",
+    release_year=2016,
+    rating=8.8
+)
+
+# Run the custom manager methods
+action_games = VideoGame.objects.games_by_genre('Action')
+recent_games = VideoGame.objects.recently_released_games(2019)
+average_rating = VideoGame.objects.average_rating()
+highest_rated = VideoGame.objects.highest_rated_game()
+lowest_rated = VideoGame.objects.lowest_rated_game()
+
+# Print the results
+print("Action games:")
+for game in action_games:
+    print(f"- {game.title} ({game.genre}, {game.release_year}, Rating: {game.rating})")
+
+print("\nRecently released games (2019+):")
+for game in recent_games:
+    print(f"- {game.title} ({game.release_year})")
+
+print(f"\nAverage rating of all games: {average_rating}")
+
+print(f"\nHighest rated game: {highest_rated.title} ({highest_rated.rating})")
+
+print(f"Lowest rated game: {lowest_rated.title} ({lowest_rated.rating})")
--- AdvancedQueriesInDjango_Exercise/main_app/models.py
+++ AdvancedQueriesInDjango_Exercise/main_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from managers import RealEstateListingManager, VideoGameManager
+from validators import rating_validator, release_year_validator
+
 
 # Create your models here.
 
@@ -18,6 +21,8 @@ class RealEstateListing(models.Model):
     bedrooms = models.PositiveIntegerField()
     location = models.CharField(max_length=100)
 
+    objects = RealEstateListingManager()
+
 
 class VideoGame(models.Model):
     GENRE_CHOICES = [
@@ -30,8 +35,10 @@ class VideoGame(models.Model):
 
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
-    release_year = models.PositiveIntegerField()
-    rating = models.DecimalField(max_digits=2,decimal_places=1)
+    release_year = models.PositiveIntegerField(validators=[release_year_validator])
+    rating = models.DecimalField(max_digits=2,decimal_places=1, validators=[rating_validator])
+
+    objects = VideoGameManager()
 
     def __str__(self):
         return self.title
@@ -45,6 +52,19 @@ class Invoice(models.Model):
     invoice_number = models.CharField(max_length=20, unique=True)
     billing_info = models.OneToOneField(BillingInfo, on_delete=models.CASCADE)
 
+    @classmethod
+    def get_invoices_with_prefix(cls, prefix: str):
+        return cls.objects.filter(invoice_number__startswith=prefix)
+
+    @classmethod
+    def get_invoices_sorted_by_number(cls):
+        return cls.objects.all().order_by('invoice_number')
+
+    @classmethod
+    def get_invoice_with_billing_info(cls, invoice_number: str):
+        return cls.objects.get(invoice_number=invoice_number)
+
+
 
 class Technology(models.Model):
     name = models.CharField(max_length=100)
@@ -56,10 +76,17 @@ class Project(models.Model):
     description = models.TextField()
     technologies_used = models.ManyToManyField(Technology, related_name='projects')
 
+    def get_programmers_with_technologies(self):
+        return self.programmers.prefetch_related('projects__technologies_used')
+
+
 
 class Programmer(models.Model):
     name = models.CharField(max_length=100)
     projects = models.ManyToManyField(Project, related_name='programmers')
+
+    def get_projects_with_technologies(self):
+        return self.projects.prefetch_related('technologies_used')
 
 
 class Task(models.Model):
